df441c9f137e06de425d1d1088d6cf6b41a53879
Murphy <smurph3iii@gmail.com>
2025-02-25 13:38:58 +1100

    Fixed the path to the main file, and ensured Chakra UI worked smoothly

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
-
-function App() {
-  return <Button>Click me</Button>;
-}
-
-export default App;
--- src/services/main.tsx
+++ src/services/main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "./components/ui/provider";
+import { ChakraProvider, Theme } from "@chakra-ui/react";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Provider>
+    <ChakraProvider>
       <App />
-    </Provider>
+    </ChakraProvider>
   </React.StrictMode>
 );
