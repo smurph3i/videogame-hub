deb324d4bdb359ebae631ed681dac3d918f7ccec
Murphy <smurph3iii@gmail.com>
2025-03-05 14:37:06 +1100

    Refactoring - Fetch games with React Query

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: '7f2bf00213594a8ba0e5f3daefb66821'
-    }
-})
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "7f2bf00213594a8ba0e5f3daefb66821",
+  },
+});
