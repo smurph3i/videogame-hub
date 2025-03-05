e4de2f54191d642a1604a03011d78e590c68aff8
Murphy <smurph3iii@gmail.com>
2025-03-05 13:27:28 +1100

    Fetched Platforms using UseQuery

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,4 +1,4 @@
-import { useQueries, useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
 import apiClient from "../services/api-client";
 import { FetchResponse } from "./useData";
