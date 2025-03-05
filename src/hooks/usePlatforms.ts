556b2d7db97a2f314529a33768e5edbc8e758f6b
Murphy <smurph3iii@gmail.com>
2025-03-05 18:33:59 +1100

    Implementing Infinite Queries in Games

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
 import APIClient from "../services/api-client";
 
@@ -15,7 +15,7 @@ const usePlatforms = () =>
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, // 24hrs
-    initialData: { count: platforms.length, results: platforms },
+    //initialData: { count: platforms.length, results: platforms },
   });
 
 export default usePlatforms;
