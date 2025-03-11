39dab44d8eda78529e1edb97dc74cd58789091ed
Murphy <smurph3iii@gmail.com>
2025-03-11 20:00:11 +1100

    Fetched Game Details

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -5,6 +5,8 @@ import { Platform } from "./usePlatforms";
 export interface Game {
   id: number;
   name: string;
+  slug: string;
+  description_raw: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
   metacritic: number;
